c10250786522973fead081c7a098c8f462659e43
szjSmiling <31468773+szjSmiling@users.noreply.github.com>
2019-04-09 14:12:18 +0800

    szj

--- multiple-page-1/src/views/About.vue
+++ multiple-page-1/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <form id="valid_form">
+
+    </form>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
